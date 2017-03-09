b2f4e5623a44889e536a70cd77f64815332dd0ce
Project EWSD <projectewsd@gmail.com>
2017-03-09 20:01:41 +0700

    v3

--- EC/src/vn/fpt/ec/actions/Login.java
+++ EC/src/vn/fpt/ec/actions/Login.java
@@ -1,5 +1,7 @@
 package vn.fpt.ec.actions;
 
+import java.util.Map;
+
 import org.apache.struts2.dispatcher.SessionMap;
 
 import vn.fpt.ec.dao.LoginDAO;
@@ -17,23 +19,28 @@ public class Login extends ActionSupport {
 	private int roleId;
 	SessionMap<String, String> sessionmap;
 
-	public String login(){
+	public String login() {
 		return SUCCESS;
 	}
-	
+
 	public String loginUser() {
 		Login login = new Login();
 		LoginDAO loginDAO = new LoginDAO();
-		login = loginDAO.login(username, pass);
-		if (login.getRoleId() == 1) {
-			return "ADMIN";
-		} else if (login.getRoleId() == 2) {
-			return "EC";
-		} else if (login.getRoleId() == 3) {
-			return "STUDENT";
-		} else {
-			return "ERROR";
+		if (!username.isEmpty() && username != null && !pass.isEmpty() && pass != null) {
+			login = loginDAO.login(username, pass);
+			if (login.getRoleId() == 1) {
+				return "ADMIN";
+			} else if (login.getRoleId() == 2) {
+				return "EC";
+			} else if (login.getRoleId() == 3) {
+				return "STUDENT";
+			} else {
+				return "ERROR";
+			}
+		}else{
+			return "LOGINERROR";
 		}
+		
 	}
 
 	public Login() {
@@ -71,4 +78,12 @@ public class Login extends ActionSupport {
 		this.roleId = roleId;
 	}
 
+	public SessionMap<String, String> getSessionmap() {
+		return sessionmap;
+	}
+
+	public void setSession(Map map) {
+		sessionmap = (SessionMap) map;
+		sessionmap.put("login", "true");
+	}
 }
